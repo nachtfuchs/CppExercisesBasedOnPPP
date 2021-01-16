53e9f38ee057808e816b47ab26552fe5edf52b6b
nachtfuchs <igor7gall+github@gmail.com>
2021-01-16 13:22:16 +0100

    - Ch08_Ex06

---
+++ Ch08_Ex06_ReverseElementsOrderString/source.cpp
@@ -0,0 +1,44 @@
+/* @brief Assignment from chapter 8 exercise 6.
+ * Book: Programming Principle and Practice using C++ - Stroustrup
+ *
+ * ASSIGNMENT:
+ * Write versions of the functions from exercise 5, but with a vector<string>.
+ */
+
+#include "../std_lib_facilities.h"
+
+vector<string> reverseVector( vector<string> inVec)
+{
+	vector<string> outVec( inVec.size() );
+	for (int idx = 0; idx < inVec.size(); idx++)
+	{
+		outVec[ idx ] = inVec[ inVec.size() - 1 - idx];
+	}
+	return outVec;
+}
+
+bool reverseVectorByRef( vector<string>& inVec )
+{
+	for (int idx = 0; idx < inVec.size() / 2; idx++ ) // use "/2" in order to "swap" once!
+	{
+		swap( inVec[ idx ], inVec[inVec.size() - 1 - idx]);
+	}
+	return true;
+}
+int main()
+{
+	vector<string> inVec { "a", "b", "c", "d", "e", "f", "g" };
+	vector<string> outVec( inVec.size() );
+
+	outVec = reverseVector( inVec );
+
+	cout << "Reverse by value:" << endl;
+	for (string x : outVec) cout << x << endl;
+	for (string x : inVec) cout << x << endl;
+
+	cout << "Reverse by reference:" << endl;
+	reverseVectorByRef( inVec );
+	for (string x : inVec) cout << x << endl;
+	keep_window_open();
+	return 0;
+}

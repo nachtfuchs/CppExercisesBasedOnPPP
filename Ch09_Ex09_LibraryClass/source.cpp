8f524229c2543acd90840ca33e1dde303c3d5970
nachtfuchs <igor7gall+github@gmail.com>
2021-02-01 20:12:50 +0100

    - Adding exercise 10.

--- Ch09_Ex09_LibraryClass/source.cpp
+++ Ch09_Ex09_LibraryClass/source.cpp
@@ -18,32 +18,55 @@
  *   signed in or not with a private member variable "signedIn". The "this"
  *   keyword was not introduced yet, but it is required to use it as part
  *   of a member function "setSignedIn()".
+ * - It is important to understand the difference between a book in the library
+ *   and another book, that might be "free floating".
  */
 
-#include "../std_lib_facilities.h"
-#include "Chrono.h"
-
+#include "Library.h"
 int main()
 {
-	Chrono::Date startDate {2020, Chrono::Month::jan, 29};
-	startDate.add_day(1);
-	cout << startDate << endl;
+	/*Check Chrono functions.*/
+//	Chrono::Date startDate {2020, Chrono::Month::jan, 29};
+//	startDate.add_day(1);
+//	cout << startDate << endl;
+//
+//	// Overrun of month
+//	startDate.add_day(2);
+//	cout << startDate << endl;
+//
+//	// Take a look at "add_month"
+//	startDate.add_month( 3 );
+//	cout << startDate << endl;
+//
+//	// Overrun of years
+//	startDate.add_month(12);
+//	cout << startDate << endl;
+//
+//	// Overrun of years using days
+//	startDate.add_day(365);
+//	cout << startDate << endl;
 
-	// Overrun of month
-	startDate.add_day(2);
-	cout << startDate << endl;
+	/*Check Library functions.*/
+	LibraryNS::Library myLibrary {};
+	Publications::Book validBook {"1234-5678-9012-A", "My valid book", "Famous Author", "22.05.2015", Publications::Genre::fiction, true};
+	myLibrary.addBook( validBook );
 
-	// Take a look at "add_month"
-	startDate.add_month( 3 );
-	cout << startDate << endl;
+	LibPerson::Patron noDebtPatron { "Bob", "Lopez", 1, 0, true};
+	myLibrary.addPatron( noDebtPatron );
 
-	// Overrun of years
-	startDate.add_month(12);
-	cout << startDate << endl;
+	LibPerson::Patron debtPatron { "Alice", "Lopez", 2, 8, true};
+	myLibrary.addPatron( debtPatron );
 
-	// Overrun of years using days
-	startDate.add_day(365);
-	cout << startDate << endl;
+	myLibrary.printInDebts();
+
+	Chrono::Date dateA {Chrono::Date (2021, Chrono::Month::feb, 1)};
+	myLibrary.checkoutBook( validBook, noDebtPatron, dateA );
+	cout << "validBook.getAvailablity(): " << validBook.getAvailability() << endl;
+
+	LibPerson::Patron signedOutPatron { "Alice", "Lopez", 2, 8, false};
+	myLibrary.addPatron( signedOutPatron );
+	Publications::Book anotherValidBook {"6548-5678-9012-B", "My new book", "Another Famous Author", "22.05.2015", Publications::Genre::nonfiction, true};
+	myLibrary.checkoutBook( anotherValidBook, signedOutPatron, Chrono::Date (2021, Chrono::Month::feb, 1) ); // Another was to provide a date.
 
 	keep_window_open();
 	return 0;
---
+++ Ch09_Ex10_DateLeapYear/source.cpp
@@ -0,0 +1,51 @@
+/* @brief Assignment from chapter 9 exercise 10.
+ * Book: Programming Principle and Practice using C++ - Stroustrup
+ *
+ * ASSIGNMENT:
+ * Implement leapyear() from §9.8.
+ *
+ * LESSONS LEARNED:
+ */
+#include "Library.h"
+
+int main()
+{
+	/*Check Chrono functions.*/
+	Chrono::Date startDate {2020, Chrono::Month::jan, 29};
+	startDate.add_day(1);
+	cout << startDate << endl;
+
+	// Overrun of month
+	startDate.add_day(2);
+	cout << startDate << endl;
+
+	// Take a look at "add_month"
+	startDate.add_month( 3 );
+	cout << startDate << endl;
+
+	// Overrun of years
+	startDate.add_month(12);
+	cout << startDate << endl;
+
+	// Overrun of years using days
+	startDate.add_day(365);
+	cout << startDate << endl;
+
+	// Test the leap years
+	Chrono::Date legalLeapYear {2004, Chrono::Month::feb, 29};
+	cout << legalLeapYear << endl;
+
+	legalLeapYear.add_day( 365 );
+	cout << legalLeapYear << endl;
+
+	Chrono::Date LeapYear400 {2000, Chrono::Month::feb, 29};
+	cout << LeapYear400 << endl;
+
+	LeapYear400.add_day( 366 );
+	cout << LeapYear400 << endl;
+
+	Chrono::Date illegalLeapYear {1900, Chrono::Month::feb, 29};
+	cout << illegalLeapYear << endl;
+	keep_window_open();
+	return 0;
+}
